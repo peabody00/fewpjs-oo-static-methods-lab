02874efadc7aee34d4b913c0fe57ef1ecdc947d1
Charlie <peabody00@gmail.com>
2021-06-16 08:51:23 -0500

    commit tree 330fb83230e5bb05c4fdb6e2860a9d77858367e7

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    return string.split(' ').map(word => {
+      if (string.indexOf(word) == 0) {
+        return this.capitalize(word);
+      } else {
+        if (exceptions.includes(word)) {
+          return word;
+        } else {
+          return this.capitalize(word);
+        }
+      }
+    }).join(' ');
+  }
 }
